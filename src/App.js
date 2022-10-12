fdc59dcb1bc008727759bbf9d16adb827a6a5942
chandan <45551851+programmerchandu@users.noreply.github.com>
2022-10-12 17:31:25 +0530

    T1
    t1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About"/>
+      <div className='container'>
+        <TextForm heading="Enter the text to analyze"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm(props) {
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+        <div className='mb-3'>
+            <textarea className="form-control" id="myBox" rows="5"></textarea>
+        </div>
+    </div>
+  )
+}
